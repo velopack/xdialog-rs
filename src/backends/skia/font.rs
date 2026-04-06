62a1509183722196582698cb96429973986a979a
Caelan <git@caesay.com>
2026-04-07 00:25:16 +0200

    Replace linux font with ubuntu

--- src/backends/skia/font.rs
+++ src/backends/skia/font.rs
@@ -1,7 +1,7 @@
 use std::sync::LazyLock;
 
-static FONT_REGULAR_DATA: &[u8] = include_bytes!("fonts/LiberationSans-Regular.ttf");
-static FONT_BOLD_DATA: &[u8] = include_bytes!("fonts/LiberationSans-Bold.ttf");
+static FONT_REGULAR_DATA: &[u8] = include_bytes!("fonts/Ubuntu-Regular.ttf");
+static FONT_BOLD_DATA: &[u8] = include_bytes!("fonts/Ubuntu-Bold.ttf");
 
 pub static FONT_REGULAR: LazyLock<fontdue::Font> = LazyLock::new(|| {
     fontdue::Font::from_bytes(FONT_REGULAR_DATA, fontdue::FontSettings::default()).unwrap()
